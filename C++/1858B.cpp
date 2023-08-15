3fdb056e51c2e3e8e2fa15764f504e00ac70bd00
Dat Hoang <q.dathoangs@gmail.com>
2023-08-15 23:16:53 +0700

    Codeforces Round 893

---
+++ C++/1858B.cpp
@@ -0,0 +1,34 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define fore(i,b,a) for(int i = b; i<a; i++)
+#define null NULL
+#define nl '\n'
+#define yes "YES\n"
+#define no "NO\n"
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cout.tie(0); cin.tie(0);
+
+    ll t; cin >> t;
+    while (t--){
+        ll n, m, d;
+
+        cin >> n >> m >> d;
+
+        ll a[m];
+
+        fore (i,0,m){
+            cin >> a[i];
+        }
+        
+        
+    }
+}
---
+++ C++/1858C.cpp
@@ -0,0 +1,41 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define fore(i,b,a) for(int i = b; i<a; i++)
+#define null NULL
+#define nl '\n'
+#define yes "YES\n"
+#define no "NO\n"
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cout.tie(0); cin.tie(0);
+
+    ll t; cin >> t;
+    while (t--){
+        ll n;
+        cin >> n;
+
+        vector <bool> chk(n+1, 0);
+
+        fore (i,1,n+1){
+            if (!chk[i]){
+                ll cur = i;
+
+                while (cur <= n){
+                    cout << cur <<  ' ';
+                    chk[cur] = true;
+                    cur *= 2;
+                }
+            }
+        }
+
+        cout << nl;
+    }
+}
